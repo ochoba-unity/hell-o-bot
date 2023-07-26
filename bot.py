61c198afe113847c6860860090b20e283e6e759c
Shakir <shakir.shaihlislamov@yandex.ru>
2023-07-26 21:24:42 +0300

    Changed var names

--- bot.py
+++ bot.py
@@ -3,10 +3,10 @@ from cfg import *
 bot = telebot.TeleBot(TOKEN)
 check_users()
 
-
+# ping command
 @bot.message_handler(commands=['ping'])
 def welcome(message):
-    bot.reply_to(message, "Пингуй лучше @pbsphp")
+    bot.reply_to(message, "Bot is up!")
 
 
 # Adds new guys
@@ -24,83 +24,64 @@ def add(message):
     bot.send_message(message.chat.id, rules)
     bot.send_photo(message.chat.id, hello_image)
 
-
+# Ping everyone in the chat
 @bot.message_handler(commands=['all', 'ALL'])
 def ping_all(message):
     all_users = read_users()
     answer = ""
-    for i in all_users:
-        if i.present:
+    for user in all_users:
+        if user.present:
             answer += "@" + i.username + "\n"
     if answer != "":
         bot.send_message(message.chat.id, answer)
     else:
         bot.send_message(message.chat.id, "Никого нет!")
-    
+
+# Adds the user to chat members list
 @bot.message_handler(commands=['add_me'])
 def add_me(message):
     all_users = read_users()
-    for i in all_users:
-        if i.username == message.from_user.username:
+    for user in all_users:
+        if user.username == message.from_user.username:
             bot.send_message(message.chat.id, "Ты уже есть")
             return
     all_users.append(Chatter(username=message.from_user.username, present=1))
     dump(all_users)
     bot.send_message(message.chat.id, "Добавила")
 
-
+# Delete left member
 @bot.message_handler(content_types=['left_chat_members'])
 def delete(message):
     all_users = read_users()
-    for i in all_users:
-        if i.username == message.left_chat_member.username:
-            i.present = 0
+    for user in all_users:
+        if user.username == message.left_chat_member.username:
+            user.present = 0
     dump(all_users)
     bot.send_message(message.chat.id, "Удалила")
 
-
-@bot.message_handler(commands=['w'])
+# Check the weather in Kazan
+@bot.message_handler(commands=['weather'])
 def get_weather(message):
     result = ''
-    w = get_page("http://www.tatarmeteo.ru/")
-    index = w.find('<div class="pogoda"><h3>Текущая погода по г. Казани</h3>')
+    page = get_page("http://www.tatarmeteo.ru/")
+    index = page.find('<div class="pogoda"><h3>Текущая погода по г. Казани</h3>')
     if index != -1:
-        w = w[index:len(w)]
-        index = w.find('</table>')
+        page = page[index:len(w)]
+        index = page.find('</table>')
         if index != -1:
-            w = w[0:index]
-            p = MyHTMLParser()
-            p.feed(w)
-            p.close()
-            result = p.data
+            page = w[0:index]
+            parser = MyHTMLParser()
+            parser.feed(page)
+            parser.close()
+            result = parser.data
         bot.send_message(message.chat.id, result)
     else:
         bot.send_message(message.chat.id, 'Weather not found!')
 
 
-class MyHTMLParser(HTMLParser):
-    def reset(self):
-        HTMLParser.reset(self)
-        self.data = ''
-
-    def handle_data(self, data):
-        self.data += data + ' '
 
 
-def get_page(url):
-    r = requests.get(url)
-    if r.status_code == 200:
-        page = r.text
-    return page
-
-
-def dump(user_list):
-    chatter_list = open(file="users.json", mode="w")
-    json.dump(user_list, fp=chatter_list, cls=ChatterEncoder)
-    chatter_list.close()
-
-
-while True:
+while __name__ == "bot":
     try:
         bot.polling()
     finally:
